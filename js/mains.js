9301e988fd34f0ef3881539f7dbe3c9ebf218d79
João Luiz Casagrande <joaoluizcasagrande@gmail.com>
2021-04-16 17:13:00 -0300

    Início JS

---
+++ js/mains.js
@@ -0,0 +1,26 @@
+var fruta = [{nome: "maça", cor: "Vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(fruta[1].nome);
+alert(fruta[1].nome);
+
+/*var lista = ["maça", "pera", "laranja"];
+lista.pop();
+lista.push("uva");
+console.log(lista);
+//console.log(lista.reverse());
+console.log(lista.length);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+
+var nome = "JOÃO LUIZ CASAGRANDE";
+var idade = 16
+//alert(nome + " tem " + idade + "anos");
+var idade2 = 14
+//alert(idade + idade2)
+var frase = "Santos é o melhor time do mundo";
+console.log(nome.toLowerCase());
+console.log(idade + idade2);
+console.log(frase.replace("Santos", "Corinthians").toUpperCase());
+//alert(frase.replace("Santos", "Corinthians"))
+*/
+
