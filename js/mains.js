ca92667330bd0bd1b686fa999dd3386eef316a91
João Luiz Casagrande <joaoluizcasagrande@gmail.com>
2021-04-17 16:28:53 -0300

    Update mains.js

--- js/mains.js
+++ js/mains.js
@@ -1,6 +1,61 @@
-var fruta = [{nome: "maça", cor: "Vermelha"}, {nome: "uva", cor: "roxa"}]
+/*function soma (n1, n2){
+    return n1 + n2;
+}
+*/
+//alert(soma(5, 10));
+
+function validaIdade (idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }
+    else{
+        validar= false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade: ");
+console.log(validaIdade(idade));
+alert(validaIdade(idade));
+
+/*function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+*/
+
+//alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+/*var d = new Date();
+alert(d.getDay() + " / " + (d.getMonth() + 1) + ", " + d.getHours() + "hrs e " + d.getMinutes() + "min");
+*/
+
+/*var cont;
+for(cont = 0; cont <= 5; cont++){
+    console.log(cont);
+    alert(cont);
+}
+*/
+
+/*var cont = 0;
+while(cont <= 5){
+    console.log(cont);
+    alert(cont);
+    cont++;
+}
+*/
+
+/*var idade = prompt("Digite sua idade: ")
+if(idade >= 18){
+    alert("Você é maior de idade");
+}
+else{
+    alert("Você é menor de idade");
+};*/
+
+/*var fruta = [{nome: "maça", cor: "Vermelha"}, {nome: "uva", cor: "roxa"}]
 console.log(fruta[1].nome);
-alert(fruta[1].nome);
+alert(fruta[1].nome);*/
 
 /*var lista = ["maça", "pera", "laranja"];
 lista.pop();
@@ -23,4 +78,3 @@ console.log(idade + idade2);
 console.log(frase.replace("Santos", "Corinthians").toUpperCase());
 //alert(frase.replace("Santos", "Corinthians"))
 */
-
